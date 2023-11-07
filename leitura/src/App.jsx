07befe1c58728be2b6e974ede9c0001781f6c43f
S-Evangelista <stefanystrenner@gmail.com>
2023-11-07 16:02:42 -0300

    Atualização
    Finalização do código, inicio do css

--- leitura/src/App.jsx
+++ leitura/src/App.jsx
@@ -1,84 +1,157 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
 function ListaDeItens() {
   const [itens, setItens] = useState([]);
   const [textoDeEntrada, setTextoDeEntrada] = useState('');
-  const [indiceItemSelecionado, setIndiceItemSelecionado] = useState(null);
-  const [resumo, setResumo] = useState('');
-  const [autor, setAutor] = useState('');
-  const [classificacao, setClassificacao] = useState(0);
+  const [edicaoDetalhes, setEdicaoDetalhes] = useState({ indice: null });
+
+  // Carregar dados salvos do localStorage ao iniciar
+  useEffect(() => {
+    const savedItens = JSON.parse(localStorage.getItem('itens'));
+    if (savedItens) {
+      setItens(savedItens);
+    }
+  }, []);
+
+  const salvarDetalhesNoLocalStorage = (itensAtualizados) => {
+    localStorage.setItem('itens', JSON.stringify(itensAtualizados));
+  };
 
   const adicionarItem = () => {
     if (textoDeEntrada.trim() !== '') {
-      setItens([...itens, { titulo: textoDeEntrada, status: 'Para Ler' }]);
+      const novoItem = {
+        titulo: textoDeEntrada,
+        status: 'Para Ler',
+        resumo: '',
+        autor: '',
+        classificacao: 0,
+      };
+      setItens([...itens, novoItem]);
       setTextoDeEntrada('');
+      salvarDetalhesNoLocalStorage([...itens, novoItem]);
     }
   };
 
-  const alternarStatus = (indice) => {
+  const alternarStatusParaLido = (indice) => {
     const itensAtualizados = [...itens];
-    const statusAtual = itensAtualizados[indice].status;
-    if (statusAtual === 'Para Ler') {
-      itensAtualizados[indice].status = 'Lendo';
-    } else if (statusAtual === 'Lendo') {
-      itensAtualizados[indice].status = 'Lido';
-    } else {
-      itensAtualizados[indice].status = 'Para Ler';
-    }
+    itensAtualizados[indice].status = 'Lido';
+    setItens(itensAtualizados);
+    salvarDetalhesNoLocalStorage(itensAtualizados);
+  };
+
+  const alternarDetalhes = (indice) => {
+    const itensAtualizados = [...itens];
+    itensAtualizados[indice].detalhesVisiveis = !itensAtualizados[indice].detalhesVisiveis;
     setItens(itensAtualizados);
   };
 
-  const lidarComCliqueNoItem = (indice) => {
-    setIndiceItemSelecionado(indice);
+  const iniciarEdicaoDetalhes = (indice) => {
+    setEdicaoDetalhes({ indice });
   };
 
-  const salvarDetalhesDoItem = () => {
-    if (indiceItemSelecionado !== null) {
-      const itensAtualizados = [...itens];
-      itensAtualizados[indiceItemSelecionado].resumo = resumo;
-      itensAtualizados[indiceItemSelecionado].autor = autor;
-      itensAtualizados[indiceItemSelecionado].classificacao = classificacao;
-      setItens(itensAtualizados);
-      setIndiceItemSelecionado(null);
-      setResumo('');
-      setAutor('');
-      setClassificacao(0);
-    }
+  const salvarEdicaoDetalhes = (indice) => {
+    const itensAtualizados = [...itens];
+    setEdicaoDetalhes({ indice: null });
+    setItens(itensAtualizados);
+    salvarDetalhesNoLocalStorage(itensAtualizados);
   };
+
+  const fecharDetalhes = (indice) => {
+    const itensAtualizados = [...itens];
+    itensAtualizados[indice].detalhesVisiveis = false;
+    setItens(itensAtualizados);
+    setEdicaoDetalhes({ indice: null });
+  };
+
+  const excluirItem = (indice) => {
+    const itensAtualizados = [...itens];
+    itensAtualizados.splice(indice, 1);
+    setItens(itensAtualizados);
+    salvarDetalhesNoLocalStorage(itensAtualizados);
+  };
+
+  const limparItens = () => {
+    setItens([]); // Limpa a lista de itens
+    localStorage.removeItem('itens'); // Remove os itens do localStorage
+  };
+
+  // Contador para itens na lista
+  const contadorItensNaLista = itens.length;
+
+  // Contador para itens marcados como Lidos
+  const contadorItensLidos = itens.filter((item) => item.status === 'Lido').length;
 
   return (
     <div>
-      <h2>Lista de Itens</h2>
+      <h2 className='titulo1'> Acervo </h2>
+      <div className='subtitulo'>
+        <p className='titulos'>Títulos: {contadorItensNaLista}</p>
+        <p className='lidos'>Lidos: {contadorItensLidos}</p>
+      </div>
       <ul>
         {itens.map((item, indice) => (
-          <li key={indice} onClick={() => lidarComCliqueNoItem(indice)}>
+          <li key={indice}>
             {item.titulo} - {item.status}
-            {indiceItemSelecionado === indice && (
+            {item.status === 'Para Ler' && (
+              <button className='marcarLido' onClick={() => alternarStatusParaLido(indice)}>Marcar como Lido</button>
+            )}
+            <button className='btDetalhes' onClick={() => alternarDetalhes(indice)}>Detalhes</button>
+            <button className='btExcluirT' onClick={() => excluirItem(indice)}>Excluir título</button>
+            {item.detalhesVisiveis && (
               <div>
-                <input
-                  type="text"
-                  placeholder="Resumo"
-                  value={resumo}
-                  onChange={(e) => setResumo(e.target.value)}
-                />
-                <input
-                  type="text"
-                  placeholder="Autor"
-                  value={autor}
-                  onChange={(e) => setAutor(e.target.value)}
-                />
-                <div>
-                  Classificação:
-                  <select value={classificacao} onChange={(e) => setClassificacao(e.target.value)}>
-                    <option value="0">Selecione a nota</option>
-                    <option value="1">1 estrela</option>
-                    <option value="2">2 estrelas</option>
-                    <option value="3">3 estrelas</option>
-                    <option value="4">4 estrelas</option>
-                    <option value="5">5 estrelas</option>
-                  </select>
-                </div>
-                <button onClick={salvarDetalhesDoItem}>Salvar Detalhes</button>
+                {edicaoDetalhes.indice === indice ? (
+                  <div>
+                    <input
+                      type="text"
+                      placeholder="Resumo"
+                      value={item.resumo}
+                      onChange={(e) => {
+                        const itensAtualizados = [...itens];
+                        itensAtualizados[indice].resumo = e.target.value;
+                        setItens(itensAtualizados);
+                      }}
+                    />
+
+                    <input
+                      type="text"
+                      placeholder="Autor"
+                      value={item.autor}
+                      onChange={(e) => {
+                        const itensAtualizados = [...itens];
+                        itensAtualizados[indice].autor = e.target.value;
+                        setItens(itensAtualizados);
+                      }}
+                    />
+
+                    <div className='classi'>
+                      Classificação:
+                      <select
+                        value={item.classificacao}
+                        onChange={(e) => {
+                          const itensAtualizados = [...itens];
+                          itensAtualizados[indice].classificacao = parseInt(e.target.value);
+                          setItens(itensAtualizados);
+                        }}
+                      >
+                        <option className='selecione' value="0">Selecione</option>
+                        <option value="⭐">⭐</option>
+                        <option value="⭐⭐">⭐⭐</option>
+                        <option value="⭐⭐⭐">⭐⭐⭐</option>
+                        <option value="⭐⭐⭐⭐">⭐⭐⭐⭐</option>
+                        <option value="⭐⭐⭐⭐⭐">⭐⭐⭐⭐⭐</option>
+                      </select>
+                    </div>
+                    <button className='btSalvar' onClick={() => salvarEdicaoDetalhes(indice)}>Salvar</button>
+                  </div>
+                ) : (
+                  <div>
+                    <p className='detalhe'>Resumo: {item.resumo}</p>
+                    <p className='detalhe'>Autor: {item.autor}</p>
+                    <p className='detalhe'>Classificação: {item.classificacao}</p>
+                    <button className='btEditar' onClick={() => iniciarEdicaoDetalhes(indice)}>Editar</button>
+                  </div>
+                )}
+                <button className='btVerMenos' onClick={() => fecharDetalhes(indice)}>Ver menos</button>
               </div>
             )}
           </li>
@@ -90,7 +163,8 @@ function ListaDeItens() {
           value={textoDeEntrada}
           onChange={(e) => setTextoDeEntrada(e.target.value)}
         />
-        <button onClick={adicionarItem}>Adicionar Item</button>
+        <button className='btAdiconar' onClick={adicionarItem}>Adicionar título</button>
+        <button className='btLimpar' onClick={limparItens}>Limpar acervo</button>
       </div>
     </div>
   );
